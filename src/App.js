5c52d0843a8267df45794357e912337fda6449af
anshikachaurasiya-cedcoss <anshikachaurasiya@cedcoss.com>
2023-05-31 13:31:45 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ComponentA from './Components/ComponentA';
+
+function App() {
+  return (
+  <div className="container">
+    <h2>TODO LIST</h2>
+    <h3>Add Item</h3>
+    <ComponentA/>
+  </div>
+  );
+}
+
+export default App;
