c48492ae4300a1bf0daeba970ade48bd9f8f354a
anshikachaurasiya-cedcoss <anshikachaurasiya@cedcoss.com>
2023-07-07 11:19:08 +0530

    todo app using class component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import ComponentA from './Components/ComponentA';
+import "./App.css";
+import ComponentA from "./Components/ComponentA";
 
 function App() {
   return (
-  <div className="container">
-    <h2>TODO LIST</h2>
-    <h3>Add Item</h3>
-    <ComponentA/>
-  </div>
+    <div className="container">
+      <h2>TODO LIST</h2>
+      <h3>Add Item</h3>
+      <ComponentA />
+    </div>
   );
 }
 
